e209c88ca4eef165e958781513219c219f785420
seiryuu <165938383+seiryuu9@users.noreply.github.com>
2026-01-17 17:50:56 +0100

    fixed reload on github pages?

--- vue-books/src/router/index.ts
+++ vue-books/src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import type { RouteRecordRaw } from 'vue-router' // typovanie pre routes
 
 const routes: RouteRecordRaw[] = [
@@ -39,7 +39,7 @@ const routes: RouteRecordRaw[] = [
 ];
 
 const router = createRouter({
-  history: createWebHistory('/book-catalogue/'), 
+  history: createWebHashHistory('/book-catalogue/'), 
   routes,
 });
 
